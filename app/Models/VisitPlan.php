f91d42c0fc02982b9d09e4ce4414db7a2e1686e4
Catur PW <caturpw@gmail.com>
2026-08-12 13:08:19 +0700

    cast-visit

--- app/Models/VisitPlan.php
+++ app/Models/VisitPlan.php
@@ -8,6 +8,10 @@ class VisitPlan extends Model
 {
     protected $fillable = ['employee_id', 'customer_id', 'visit_date', 'status'];
 
+    protected $casts = [
+        'visit_date' => 'date',
+    ];
+
     public function employee()
     {
         return $this->belongsTo(Employee::class);
